02dd498a1faf70156e241545533cde6e9b83d0a1
Tien Nguyen Hoang Tran <Tntran@marinaco.com>
2023-07-04 16:07:58 +0700

    up

---
+++ smoothing.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+img = cv.imread('Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+# Averaging
+average = cv.blur(img, (3,3))
+cv.imshow('Average Blur', average)
+
+# Gaussian Blur
+gauss = cv.GaussianBlur(img, (3,3), 0)
+cv.imshow('Gaussian Blur', gauss)
+
+# Median Blur
+median = cv.medianBlur(img, 3)
+cv.imshow('Median Blur', median)
+
+cv.waitKey(0)
